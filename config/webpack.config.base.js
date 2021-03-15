c619a8cbd8ba04d8067e4b0ac7b58294604ba1da
wangyouxun <weedwong@outlook.com>
2021-03-15 10:48:24 +0800

    init

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -14,7 +14,7 @@ module.exports = {
                 test: '/\.(js|jsx)$/',
                 // 用 babel-loader 转换 JavaScript 文件
                 // ?cacheDirectory 表示传给 babel-loader 的参数，用于缓存 babel 编译结果加快重新编译速度
-                use: ['babel-loader'],
+                use: ['babel-loader']
                 // 只命中src目录里的js文件，加快 Webpack 搜索速度
                 // include: path.resolve(__dirname, './src')
               },
